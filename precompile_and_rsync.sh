4ddefd7b67795b4d6b20755ec6ed3597f41c85af
Ben Gibbs <ben@bobop.co.uk>
2014-06-23 16:30:18 +0100

    removed need for DB

--- precompile_and_rsync.sh
+++ precompile_and_rsync.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# touch /cloud9/logs/$1.log
-# exec > >(tee /cloud9/logs/$1.log)
-# exec 2>&1
+exec > >(tee /cloud9/logs/all.log)
+exec 2>&1
 
 cd /cloud9/precompiled_assets/oliver-precompile
 touch /cloud9/logs/$1.log
