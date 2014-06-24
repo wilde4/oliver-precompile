21a829268a73bd43ab1c69c17b4e233045fc643c
Ben Gibbs <ben@bobop.co.uk>
2014-06-24 16:29:39 +0100

    shell changes

--- precompile_and_rsync.sh
+++ precompile_and_rsync.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+# LOGGING
 exec > >(tee /cloud9/logs/all.log)
 exec 2>&1
 
-cd /cloud9/precompiled_assets/oliver-precompile
+# whoami
+
+# RUN PRECOMPILE
 touch /cloud9/logs/$1.log
-RAILS_ENV=production THEME_NAME=$1 bundle exec rake assets:precompile
+cd /cloud9/precompiled_assets/oliver-precompile
+RAILS_ENV=production THEME_NAME=$1 bundle exec rake assets:precompile > /cloud9/logs/$1.log
+
+# COPY PRECOMPILED ASSETS TO /cloud9/sync/precompiled_assets
+cp -R /cloud9/precompiled_assets/$1 /cloud9/sync/precompiled_assets/$2/$1
+
+# COPY LIQUID FILES TO /cloud9/sync/themes/staging OR /cloud9/sync/themes/production
+cp -R /cloud9/cloud9/$1 /cloud9/sync/themes/$2/$1
+
+# RSYNC FILES TO oliver-production STACK INSTANCES IN Rails App Server LAYER
+# rails-app2: 10.80.166.218
+rsync -avz -e ssh  /cloud9/sync deploy@10.80.166.218:/srv/www/oliver/shared/system
