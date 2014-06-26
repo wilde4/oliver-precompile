fa2f1c196c6a2e3a126d4c94a01178096c053371
Ben Gibbs <ben@bobop.co.uk>
2014-06-26 15:42:17 +0100

    added rm commands to shell script

--- precompile_and_rsync.sh
+++ precompile_and_rsync.sh
@@ -12,10 +12,12 @@ cd /cloud9/precompiled_assets/oliver-precompile
 RAILS_ENV=production THEME_NAME=$1 bundle exec rake assets:precompile > /cloud9/logs/$1.log
 
 # COPY PRECOMPILED ASSETS TO /cloud9/sync/precompiled_assets
-cp -R /cloud9/precompiled_assets/$1 /cloud9/sync/precompiled_assets/$2
+rm -rf /cloud9/sync/precompiled_assets/$2/$1
+cp -Rf /cloud9/precompiled_assets/$1 /cloud9/sync/precompiled_assets/$2
 
 # COPY LIQUID FILES TO /cloud9/sync/themes/staging OR /cloud9/sync/themes/production
-cp -R /cloud9/cloud9/$1 /cloud9/sync/themes/$2
+rm -rf /cloud9/sync/themes/$2/$1
+cp -Rf /cloud9/cloud9/$1 /cloud9/sync/themes/$2
 
 # RSYNC FILES TO oliver-production STACK INSTANCES IN Rails App Server LAYER
 # rails-app2: 10.80.166.218
