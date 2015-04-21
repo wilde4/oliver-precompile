9220063947306ab02cc7e4bf3c0d0bcf41786f15
Ben Gibbs <ben@bobop.co.uk>
2015-04-21 16:05:04 +0100

    fixed public path in precompile.sh

--- precompile.sh
+++ precompile.sh
@@ -13,7 +13,7 @@ RAILS_ENV=production THEME_NAME=$1 MODE=$2 bundle exec rake assets:precompile
 
 # COPY PRECOMPILED ASSETS TO /srv/sync/precompiled_assets
 rm -rf /srv/www/precompile_app/shared/sync/precompiled_assets/$2/$1
-cp -Rf /srv/www/precompile_app/public/$2/$1 /srv/www/precompile_app/shared/sync/precompiled_assets/$2
+cp -Rf /srv/www/precompile_app/current/public/$2/$1 /srv/www/precompile_app/shared/sync/precompiled_assets/$2
 
 # COPY LIQUID FILES TO /srv/www/precompile_app/shared/sync/themes/staging OR /srv/www/precompile_app/shared/sync/themes/production
 rm -rf /srv/www/precompile_app/shared/sync/themes/$2/$1
