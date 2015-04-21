84c4ee0072b39a9d35401f5509dc2bd24c40e24d
Ben Gibbs <ben@bobop.co.uk>
2015-04-21 15:54:32 +0100

    added rubyracer and altered sync dir paths

--- precompile.sh
+++ precompile.sh
@@ -12,9 +12,9 @@ RAILS_ENV=production THEME_NAME=$1 MODE=$2 bundle exec rake assets:clean
 RAILS_ENV=production THEME_NAME=$1 MODE=$2 bundle exec rake assets:precompile
 
 # COPY PRECOMPILED ASSETS TO /srv/sync/precompiled_assets
-rm -rf /srv/sync/precompiled_assets/$2/$1
-cp -Rf /srv/www/precompile_app/public/$2/$1 /srv/sync/precompiled_assets/$2
+rm -rf /srv/www/precompile_app/shared/sync/precompiled_assets/$2/$1
+cp -Rf /srv/www/precompile_app/public/$2/$1 /srv/www/precompile_app/shared/sync/precompiled_assets/$2
 
-# COPY LIQUID FILES TO /srv/sync/themes/staging OR /srv/sync/themes/production
-rm -rf /srv/sync/themes/$2/$1
-cp -Rf /srv/www/volcanic_deploy/shared/themes/$1 /srv/sync/themes/$2
+# COPY LIQUID FILES TO /srv/www/precompile_app/shared/sync/themes/staging OR /srv/www/precompile_app/shared/sync/themes/production
+rm -rf /srv/www/precompile_app/shared/sync/themes/$2/$1
+cp -Rf /srv/www/volcanic_deploy/shared/themes/$1 /srv/www/precompile_app/shared/sync/themes/$2
