e33e6de78c2452c044b5e83ec2612e7c6efaad46
Andy Turner <a.turner@volcanic.co.uk>
2015-07-07 09:35:54 +0100

    New rsync approach to speed up exporting

--- precompile_and_rsync.sh
+++ precompile_and_rsync.sh
@@ -22,7 +22,11 @@ cp -Rf /cloud9/cloud9/$1 /cloud9/sync/themes/$2
 
 # RSYNC FILES TO oliver-production STACK INSTANCES IN Rails App Server LAYER
 
+# Instance Type: c3.large
+rsync -arvce "ssh -o StrictHostKeyChecking=no" /cloud9/sync/precompiled_assets/$2/$1 deploy@ec2-54-75-239-163.eu-west-1.compute.amazonaws.com:/srv/www/oliver/shared/sync/precompiled_assets/$2
+rsync -arvce "ssh -o StrictHostKeyChecking=no" /cloud9/sync/themes/$2/$1 deploy@ec2-54-75-239-163.eu-west-1.compute.amazonaws.com:/srv/www/oliver/shared/sync/themes/$2
 # Instance Type: m3.large
-rsync -arvce "ssh -o StrictHostKeyChecking=no" /cloud9/sync deploy@ec2-54-75-239-163.eu-west-1.compute.amazonaws.com:/srv/www/oliver/shared
-# Instance Type: m3.large
-rsync -arvce "ssh -o StrictHostKeyChecking=no" /cloud9/sync deploy@ec2-54-75-194-77.eu-west-1.compute.amazonaws.com:/srv/www/oliver/shared
+rsync -arvce "ssh -o StrictHostKeyChecking=no" /cloud9/sync/precompiled_assets/$2/$1 deploy@ec2-54-75-199-235.eu-west-1.compute.amazonaws.com:/srv/www/oliver/shared/sync/precompiled_assets/$2
+rsync -arvce "ssh -o StrictHostKeyChecking=no" /cloud9/sync/themes/$2/$1 deploy@ec2-54-75-199-235.eu-west-1.compute.amazonaws.com:/srv/www/oliver/shared/sync/themes/$2
+
+curl -X PUT -H "Content-Type: application/json" -d '{"theme_name": "'"$1"'"}' https://www.volcanic.co.uk/api/v1/metacontents/update_timestamps.json 
