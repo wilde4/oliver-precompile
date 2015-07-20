c20d0aa5ade98abea52cfc8699ec98f09f2655a2
Ben Gibbs <ben@bobop.co.uk>
2015-07-20 12:05:05 +0100

    further rsync_then_deploy changes

--- rsync_then_deploy.sh
+++ rsync_then_deploy.sh
@@ -11,7 +11,7 @@ exec 2>&1
 # whoami
 
 echo "---------------"
-echo "Starting Deploy to $2 for $1"
+echo "Starting RSYNC to $2 for $1"
 echo "---------------"
 
 # START RSYNC OF THEME FILES TO VOLCANIC DEPLOY BOX
@@ -20,5 +20,13 @@ echo "---------------"
 # NEED TO RSYNC TO VOLCANIC DEPLOY BOX /srv/www/volcanic_deploy/shared/themes/mode_name SO PRECOMPILE APP CAN ACCESS FILES FOR PRECOMPILE PROCESS WITH ASSET PATH /srv/www/volcanic_deploy/shared/themes/#{ENV["MODE"]}/#{ENV["THEME_NAME"]}/assets/
 rsync -arvce "ssh -o StrictHostKeyChecking=no" --delete /cloud9/cloud9/$1 deploy@46.137.112.6:/srv/www/volcanic_deploy/shared/themes/$2
 
+echo "---------------"
+echo "Starting Deploy API request to $2 for $1"
+echo "---------------"
+
 # CURL TO VOLCANIC DEPLOY API TO START DEPLOY PROCESS
 curl -X POST -H "Content-Type: application/json" -d '{ "deploy": { "mode": "'"$2"'", "theme": "'"$1"'" } }' http://deploy.volcanic.co.uk/api/v1/deploys.json
+
+echo "---------------"
+echo "Finished RSYNC and Deploy API request to $2 for $1"
+echo "---------------"
