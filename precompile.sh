dbd4ac5fbb69f18385d9db1e273e36f313c7307c
Ben Gibbs <ben@bobop.co.uk>
2016-07-25 10:37:09 -0700

    added private IP to precompile.sh

--- precompile.sh
+++ precompile.sh
@@ -13,24 +13,18 @@ echo "---------------"
 
 # ORIGINAL THEME FILES ON DEV BOX /cloud9/cloud9/theme_name
 # NEED TO RSYNC TO VOLCANIC DEPLOY BOX /srv/www/volcanic_deploy/shared/themes/mode_name SO PRECOMPILE APP CAN ACCESS FILES FOR PRECOMPILE PROCESS WITH ASSET PATH /srv/www/volcanic_deploy/shared/themes/#{ENV["MODE"]}/#{ENV["THEME_NAME"]}/assets/
-rsync -arvce "ssh -o StrictHostKeyChecking=no" --delete deploy@54.75.232.48:/cloud9/cloud9/$1 /srv/www/volcanic_deploy/shared/themes/$2
+# rsync -arvce "ssh -o StrictHostKeyChecking=no" --delete deploy@54.75.232.48:/cloud9/cloud9/$1 /srv/www/volcanic_deploy/shared/themes/$2
+rsync -arvce "ssh -o StrictHostKeyChecking=no" --delete deploy@10.0.1.200:/cloud9/cloud9/$1 /srv/www/volcanic_deploy/shared/themes/$2
 
 # RUN PRECOMPILE
 cd /srv/www/precompile_app/current
 RAILS_ENV=production THEME_NAME=$1 MODE=$2 bundle exec rake assets:clean
 RAILS_ENV=production THEME_NAME=$1 MODE=$2 bundle exec rake assets:precompile
 
-# COPY PRECOMPILED ASSETS TO /srv/sync/precompiled_assets
-# rm -rf /srv/www/precompile_app/shared/sync/precompiled_assets/$2/$1
-# cp -Rf /srv/www/precompile_app/current/public/$2/$1 /srv/www/precompile_app/shared/sync/precompiled_assets/$2
 # COPY PRECOMPILED ASSETS TO /deploy_themes/precompiled_assets
 rm -rf /deploy_themes/precompiled_assets/$2/$1
 cp -Rf /srv/www/precompile_app/current/public/$2/$1 /deploy_themes/precompiled_assets/$2
 
-
-# COPY LIQUID FILES TO /srv/www/precompile_app/shared/sync/themes/staging OR /srv/www/precompile_app/shared/sync/themes/production
-# rm -rf /srv/www/precompile_app/shared/sync/themes/$2/$1
-# cp -Rf /srv/www/volcanic_deploy/shared/themes/$2/$1 /srv/www/precompile_app/shared/sync/themes/$2
 # COPY LIQUID FILES TO /deploy_themes/themes/staging OR /deploy_themes/themes/production
 rm -rf /deploy_themes/themes/$2/$1
 cp -Rf /srv/www/volcanic_deploy/shared/themes/$2/$1 /deploy_themes/themes/$2
@@ -39,15 +33,11 @@ cp -Rf /srv/www/volcanic_deploy/shared/themes/$2/$1 /deploy_themes/themes/$2
 IFS=','
 for dns in $4
 do
-  # rsync -arvce "ssh -o StrictHostKeyChecking=no" --delete /srv/www/precompile_app/shared/sync/precompiled_assets/$2/$1 deploy@$dns:/srv/www/oliver/shared/sync/precompiled_assets/$2
-  # rsync -arvce "ssh -o StrictHostKeyChecking=no" --delete /srv/www/precompile_app/shared/sync/themes/$2/$1 deploy@$dns:/srv/www/oliver/shared/sync/themes/$2
   rsync -arvce "ssh -o StrictHostKeyChecking=no" --delete /deploy_themes/precompiled_assets/$2/$1 deploy@$dns:/srv/www/oliver/shared/sync/precompiled_assets/$2
   rsync -arvce "ssh -o StrictHostKeyChecking=no" --delete /deploy_themes/themes/$2/$1 deploy@$dns:/srv/www/oliver/shared/sync/themes/$2
 done
 
 # s3cmd PUSH TO S3 BUCKET
-# s3cmd sync --recursive --delete-removed /srv/www/precompile_app/shared/sync/precompiled_assets/$2/$1/ s3://oliver-themes/precompiled_assets/$2/$1/ >/tmp/s3_put_errors.txt
-# s3cmd sync --recursive --delete-removed /srv/www/precompile_app/shared/sync/themes/$2/$1/ s3://oliver-themes/themes/$2/$1/ >/tmp/s3_put_errors.txt
 s3cmd sync --recursive --delete-removed /deploy_themes/precompiled_assets/$2/$1/ s3://oliver-themes/precompiled_assets/$2/$1/ >/tmp/s3_put_errors.txt
 s3cmd sync --recursive --delete-removed /deploy_themes/themes/$2/$1/ s3://oliver-themes/themes/$2/$1/ >/tmp/s3_put_errors.txt
 
