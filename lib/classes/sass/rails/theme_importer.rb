623285d30d1e4a44b3d24a3f04e7d447d4f2968f
Matthew Whiteley <m.whiteley@volcanic.co.uk>
2017-02-24 15:54:06 +0000

    correct import location

--- lib/classes/sass/rails/theme_importer.rb
+++ lib/classes/sass/rails/theme_importer.rb
@@ -12,9 +12,9 @@ module Sass
         # puts options.inspect
         if uri.start_with?('$theme')
           uri = uri.sub('$theme/', '')
-          full_filename = ::Rails.root.to_s + "/test-themes/#{ENV['THEME_NAME']}/assets/stylesheets/#{uri}.scss"
+          # full_filename = ::Rails.root.to_s + "/test-themes/#{ENV['THEME_NAME']}/assets/stylesheets/#{uri}.scss"
           
-          # full_filename = "/srv/www/volcanic_deploy/shared/themes/#{ENV["MODE"]}/#{ENV["THEME_NAME"]}/assets/stylesheets/#{uri}.scss"
+          full_filename = "/srv/www/volcanic_deploy/shared/themes/#{ENV["MODE"]}/#{ENV["THEME_NAME"]}/assets/stylesheets/#{uri}.scss"
 
           Sass::Engine.new(File.read(full_filename), options)
         else
