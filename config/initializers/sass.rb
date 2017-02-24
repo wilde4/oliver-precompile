839a3974cf55a64eaa6ec1f6cbab886130178d29
Matthew Whiteley <m.whiteley@volcanic.co.uk>
2017-02-24 10:29:37 +0000

    remove unneeded require

--- config/initializers/sass.rb
+++ config/initializers/sass.rb
@@ -1,6 +1,5 @@
 # Sass.load_paths << Sass::Importers::ThemeImporter.new
 require 'sass'
 require 'sass/plugin'
-require 'sass/rails/theme_importer'
 # Sass::Plugin.options[:load_paths] << Sass::Rails::ThemeImporter.new
 Sass.load_paths << Sass::Rails::ThemeImporter.new
